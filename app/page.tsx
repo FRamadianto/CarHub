16b0304e705c74408353526463cac3ce722720a1
Fikri Ramadianto <ramadiantofikri@gmail.com>
2024-03-29 02:41:06 +0700

    implement show more button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
 export default async function Home({ searchParams }) {
@@ -35,6 +35,10 @@ export default async function Home({ searchParams }) {
                 <CarCard key={car.id} car={car} />
               ))}
             </div>
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="error-container">
